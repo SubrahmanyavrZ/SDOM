56f7c7d3fc5024825cbe62fb854886a49833ecb9
Subrahmanya.vr <125297754+SubrahmanyavrZ@users.noreply.github.com>
2024-10-20 12:00:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const customButton = document.createElement("custom-sdom")
-const shadowRoot = customButton.attachShadow({ mode: "open" })
-shadowRoot.innerHTML = `
-  <h1> Shadow DOM Header
-    <button> Shadow button </button>
-  </h1>
-  <style>h1 { color: grey } button { color: grey }</style>`
-document.body.append(customButton)
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const customButton = document.createElement("custom-sdom");
+    const shadowRoot = customButton.attachShadow({ mode: "open" });
+    shadowRoot.innerHTML = `
+      <h1> Shadow DOM Header
+        <button> Shadow button </button>
+      </h1>
+      <style>h1 { color: grey } button { color: grey }</style>`;
+    document.body.append(customButton);
+  });
+</script>
